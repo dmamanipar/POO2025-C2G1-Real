7dc79d466bca02dd9b664e670fb824a31a17bae2
dmamanipar <dmamanipar@unsa.edu.pe>
2025-09-05 16:29:03 -0500

    lista en table view y datos en conbobox

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
@@ -1,8 +1,54 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import pe.edu.upeu.asistencia.enums.Carrera;
+import pe.edu.upeu.asistencia.enums.TipoParticipante;
+import pe.edu.upeu.asistencia.modelo.Participante;
+import pe.edu.upeu.asistencia.servicio.ParticipanteServicioI;
 
 @Controller
 public class ParticipanteController {
+
+    @FXML
+    private ComboBox<Carrera> cbxCarrera;
+
+    @FXML
+    private ComboBox<TipoParticipante> cbxTipoParticipante;
+
+    @FXML
+    private TableView<Participante> tableRegPart;
+    ObservableList<Participante> participantes;
+
+    @Autowired
+    ParticipanteServicioI ps;
+
+    @FXML
+    public void initialize(){
+        cbxCarrera.getItems().addAll(Carrera.values());
+        cbxTipoParticipante.getItems().addAll(TipoParticipante.values());
+
+        cbxCarrera.getSelectionModel().select(4);
+        Carrera carrera = cbxCarrera.getSelectionModel().getSelectedItem();
+        System.out.println(carrera.name());
+        listarPartipantes();
+    }
+
+    public void listarPartipantes(){
+        TableColumn<Participante, String> dniCol = new TableColumn<>("DNI");
+        dniCol.setCellValueFactory(cellData -> cellData.getValue().getDni());
+
+        participantes = FXCollections.observableList(ps.findAll());
+        tableRegPart.getColumns().addAll(dniCol);
+        tableRegPart.setItems(participantes);
+    }
+
 
 }
